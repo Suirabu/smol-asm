a4c7d192cd2637010716aa3243ca83f4215cc6ec
Suirabu <suirabu.dev@gmail.com>
2022-05-13 22:26:45 -0700

    Implement basic file I/O to read source from files

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, SmolAsm!\n");
-    return 0;
+#include "error.h"
+#include "util.h"
+
+int main(int argc, char** argv) {
+    if(argc < 2) {
+        REPORT_ERROR("No input file provided\n");
+        return EXIT_FAILURE;
+    }
+
+    const char* source_path = argv[1];
+    
+    char* source = util_get_file_contents(source_path);
+    if(!source) {
+        return EXIT_FAILURE;
+    }
+
+    printf("File contents:\n%s\n", source);
+    free(source);
+
+    return EXIT_SUCCESS;
 }
